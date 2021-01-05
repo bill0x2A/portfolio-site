66d3f4a7939ddbade192cd20b18cf2b138e62ba5
bill0x2A <billysmith1998@live.com>
2021-01-05 21:44:01 +0000

    Spelling

--- src/containers/Main/TextContainer/TextContainer.js
+++ src/containers/Main/TextContainer/TextContainer.js
@@ -11,7 +11,7 @@ I worked at a pub and went travelling until the COVID-19 pandemic began.</p>
 to learn more but didn’t want to spend more money on education.</p>
 <p>I used lockdown as an opportunity to pick up HTML, CSS and JS.
 Within two months, I managed to find my first two clients and I became
-familliar with Node and Express to facilitate thier needs.</p>
+familliar with Node and Express to facilitate their needs.</p>
 <p>Wanting to build more than static websites, I began learning React in
 July 2020 and since then I have furthered my skills through a series of projects - showcased below.</p>
 {props.children}
