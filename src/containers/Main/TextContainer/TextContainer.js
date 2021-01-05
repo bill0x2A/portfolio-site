e65c1d7b9327dce7cc82968bc1082bf875ecbd6a
bill0x2A <billysmith1998@live.com>
2021-01-05 21:21:31 +0000

    Grammar & spelling

--- src/containers/Main/TextContainer/TextContainer.js
+++ src/containers/Main/TextContainer/TextContainer.js
@@ -6,13 +6,13 @@ const textcontainer = (props) => {
     <div className = {classes.TextContainer}>
         <h2>MY STORY</h2>
         <p>After completing my physics degree at Bristol University in 2019,
-I worked at a pub and went travelling until COVID-19 began.</p>
+I worked at a pub and went travelling until the COVID-19 pandemic began.</p>
 <p>After writing a crucial part of my final year project in Python, I knew I wanted
 to learn more but didn’t want to spend more money on education.</p>
-<p>I used lockdown as an oppertunity to pick up HTML, CSS and JS.
-Within two months I managed to find my first two clients, and I became
+<p>I used lockdown as an opportunity to pick up HTML, CSS and JS.
+Within two months, I managed to find my first two clients and I became
 familliar with Node and Express to facilitate thier needs.</p>
-<p>Wanting to build more than static websites I began learning React in
+<p>Wanting to build more than static websites, I began learning React in
 July 2020 and since then I have furthered my skills through a series of projects - showcased below.</p>
 {props.children}
     </div>
